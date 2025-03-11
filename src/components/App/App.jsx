cff762c8a09efb7d6eccdffa837bb8e9eab3630d
unknown <golovinov817@gmail.com>
2025-03-11 21:50:55 +0100

    home work

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import LoginForm from '../LoginForm/LoginForm';
+
+const App = () => {
+
+    // Колбек-функція для обробки сабміту форми
+    const handleLogin = (userData) => {
+      // Виконуємо необхідні операції з даними
+      console.log(userData);
+    };
+  
+    return (
+      <div>
+        <h1>Please login to your account!</h1>
+        {/* Передаємо колбек як пропс форми */}
+        <LoginForm onLogin={handleLogin} />
+      </div>
+    );
+  };
+  
+ export default App;
---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,47 @@
+// const LoginForm = ({ onLogin }) => {
+
+//   const handleSubmit = (evt) => {
+//     evt.preventDefault();
+
+//     const form = evt.target;
+//     const { login, password } = form.elements;
+
+//     // Викликаємо пропс onLogin
+//     onLogin({
+//       login: login.value,
+//       password: password.value,
+//     });
+
+//     form.reset();
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input type="text" name="login" />
+//       <input type="password" name="password" />
+//       <button type="submit">Login</button>
+//     </form>
+//   );
+// };
+
+import { useId } from 'react';
+
+const LoginForm = () => {
+  const loginId = useId();
+  const passwordId = useId();
+
+  return (
+    <form>
+      <label htmlFor={loginId}>Login</label>
+      <input type="text" name="login" id={loginId} />
+
+      <label htmlFor={passwordId}>Password</label>
+      <input type="password" name="password" id={passwordId} />
+
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+
+  export default LoginForm;
